5f3772a37d1312de9b4d12b8df95c30df04859f3
Alcalixo <rodrix_gonzalo@outlook.com>
2022-07-25 18:27:00 -0300

    Creación del Header - Parte 1
    Creación de módulos LoginButton, Social, y Header (que contrendrá ambos, junto con el banner - a crear -, y el logo de ArgentinaPrograma)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SocialComponent } from './components/social/social.component';
+import { LoginButtonComponent } from './components/login-button/login-button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SocialComponent,
+    LoginButtonComponent
   ],
   imports: [
     BrowserModule,
