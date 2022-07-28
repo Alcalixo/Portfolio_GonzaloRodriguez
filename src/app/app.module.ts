d6eedbb9a571ea84c956a1d7942709b36ba92529
Alcalixo <rodrix_gonzalo@outlook.com>
2022-07-28 18:37:35 -0300

    Creación de sección Proyectos, queda alinear los botones al centro de la tarjeta

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { ExperienciaComponent } from './components/experiencia/experiencia.compo
 import { EducacionComponent } from './components/educacion/educacion.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
 import { HardsoftskillComponent } from './components/hardsoftskill/hardsoftskill.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
 
 @NgModule({
   declarations: [
@@ -23,7 +24,8 @@ import { HardsoftskillComponent } from './components/hardsoftskill/hardsoftskill
     AboutComponent,
     ExperienciaComponent,
     EducacionComponent,
-    HardsoftskillComponent
+    HardsoftskillComponent,
+    ProyectosComponent
   ],
   imports: [
     BrowserModule,
