80ecb5ad0c3a82604fd753e2e6c88dc80393d2a0
Alcalixo <rodrix_gonzalo@outlook.com>
2022-08-05 19:21:56 -0300

    creando estructura del backend del proyecto integrador

--- BACKEND/src/main/java/com/portfolio/gnr/GnrApplication.java
+++ BACKEND/src/main/java/com/portfolio/gnr/GnrApplication.java
@@ -1,4 +1,4 @@
-package com.portfolio.argentinaprograma;
+package com.portfolio.gnr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- BACKEND/src/test/java/com/portfolio/gnr/GnrApplicationTests.java
+++ BACKEND/src/test/java/com/portfolio/gnr/GnrApplicationTests.java
@@ -1,4 +1,4 @@
-package com.portfolio.argentinaprograma;
+package com.portfolio.gnr;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
