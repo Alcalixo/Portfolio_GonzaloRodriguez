a7f57c60a7bbbb42ec2b54f4ae7af6683d747b1c
Alcalixo <rodrix_gonzalo@outlook.com>
2022-08-05 22:51:37 -0300

    BackEnd, por el momento, corriendo correctamente, realizar pruebas

--- BACKEND/src/main/java/com/portfolio/gnr/Controller/PersonaController.java
+++ BACKEND/src/main/java/com/portfolio/gnr/Controller/PersonaController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class PersonaController {
     @Autowired IPersonaService ipersonaservice;
     
-    @GetMapping ("/persona/traer")
+    @GetMapping ("/personas/traer")
     public List<Persona> getPersona(){
         return ipersonaservice.getPersona();
     }
