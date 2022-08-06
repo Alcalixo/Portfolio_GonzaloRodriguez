12cc4004f8afa6fae2b30f38bb6f9de54bffd818
Alcalixo <rodrix_gonzalo@outlook.com>
2022-08-05 22:24:36 -0300

    intentando corregir errores de lanzamiento

--- BACKEND/src/main/java/com/portfolio/gnr/Entity/Persona.java
+++ BACKEND/src/main/java/com/portfolio/gnr/Entity/Persona.java
@@ -5,10 +5,12 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter @Setter
+
 @Entity
 public class Persona {
     
@@ -17,11 +19,12 @@ public class Persona {
     private Long id;
     
     @NotNull
+    @Size (min = 1, max = 50, message = "Debe ingresar un valor válido")
     private String nombre;
     @NotNull
+    @Size (min = 1, max = 50, message = "Debe ingresar un valor válido")
     private String apellido;
+    @Size (min = 1, max = 50, message = "Debe ingresar un valor válido")
     private String img;
-    
-    
     
 }
