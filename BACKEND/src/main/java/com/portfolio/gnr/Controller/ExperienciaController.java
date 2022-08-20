c380999e46634517cdaacb8dbe1ff991a65cb18f
Alcalixo <rodrix_gonzalo@outlook.com>
2022-08-20 20:46:18 -0300

    intentando crear usuarios, falla conexión con servidor

--- BACKEND/src/main/java/com/portfolio/gnr/Controller/ExperienciaController.java
+++ BACKEND/src/main/java/com/portfolio/gnr/Controller/ExperienciaController.java
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("explab")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://portfoliognr-argentinaprograma.web.app")
 public class ExperienciaController {
 
     @Autowired
