3f6b33d08b2dc16b6c0dc4f494df729ee06b0e28
Alcalixo <rodrix_gonzalo@outlook.com>
2022-08-20 19:38:41 -0300

    cambio de rutas, para que tome la host de heroku del backend

--- FRONTEND/src/app/service/educacion.service.ts
+++ FRONTEND/src/app/service/educacion.service.ts
@@ -7,7 +7,7 @@ import { Educacion } from '../model/educacion';
   providedIn: 'root'
 })
 export class EducacionService {
-  educacionURL = 'http://localhost:8080/educacion/';
+  educacionURL = 'https://argentina-programa-gnr.herokuapp.com/educacion/';
 
   constructor(private httpClient: HttpClient) { }
 
